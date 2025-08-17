da33340719cadb24388bab0c1ea97d2e092d8f07
2400030373 <2400030373@kluniversity.in>
2025-08-17 18:39:19 +0530

    calculator

---
+++ Example1.js
@@ -0,0 +1,28 @@
+// var city ="London"
+// let country ="UK"
+// const pi =3.14;
+// console.log(country)
+// console.log(city)
+
+//let a1 = 100
+//let a2 = 200
+//console.log(a1+a2)
+
+// let sum = 5+3;
+// let isEqual =(sum===8);
+// console.log(isEqual)
+
+// let assignVar=10;
+// assignVar+=5;
+// console.log(assignVar)
+
+// let attendence=80+5;
+// let eligibleForExam=(attendence<=75);
+// console.log("Eligible for exam?",eligibleForExam);
+
+function greetStudent(name)
+{
+    return "Welcome " + name+ "to KL University";
+}
+console.log(greetStudent("Juber "));
+
